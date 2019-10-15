02d79758b941291ae0b7635d56dd2d5e607c3ce8
Wilson Y. Wong <Wilson.Young.Wong@gmail.com>
2019-10-14 22:07:36 -0700

    Update validation for Add Stock Transaction
    * Add validation for currency, positive integers, and date field
    * cleanup code
    * some minor styling to make the inputs less ugly

--- src/components/StockTransactionsContainer.js
+++ src/components/StockTransactionsContainer.js
@@ -3,6 +3,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { addTransaction } from "../actions/StockTransaction";
 import { Button, Input, Modal, Dropdown } from 'semantic-ui-react'
+import './StockTransactionsContainer.css';
 
 
 const mapToStateProps = function(state) {
@@ -22,58 +23,73 @@ const transactionTypes = [
   { key: '1', value: '1', text: 'Sell' },
 ];
 
+const addTransactionFormInitialState = {
+  transactionType: '0',
+  stock_symbol: "",
+  cost_per_unit: "",
+  quantity: "",
+  trade_fee: "",
+  trade_date: "",
+  valid_date: false
+}
+
+const validCurrency = (value) => {
+  return /^\d+(\.\d{0,2}?)?$/.test(value);
+}
+
+const currencyToInteger = (value) => {
+  return Math.round(parseFloat(value) * 100);
+}
+
+const validPositiveInteger = (value) => {
+  return /^\d+$/.test(value);
+}
+
+const validYYYY_MM_DD = (value) => {
+  return /^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/.test(value);
+}
+
 class StockTransactionsContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       open: false,
-      transactionType: '0',
+      addTransactionFields: Object.assign(
+        {}, addTransactionFormInitialState
+      )
     };
     this.show = () => this.setState({ open: true })
     this.close = () => this.setState({ open: false })
-    this.handleTransactionTypeChange = (e, { value }) => this.setState({ transactionType: value });
+    this.transactionType = this.transactionType.bind(this);
+    this.handleTransactionTypeChange = this.handleTransactionTypeChange.bind(this);
+    this.stockSymbol = this.stockSymbol.bind(this);
+    this.handleStockSymbolChange = this.handleStockSymbolChange.bind(this);
+    this.costPerUnit = this.costPerUnit.bind(this);
+    this.handleCostPerUnitChange = this.handleCostPerUnitChange.bind(this);
+    this.quantity = this.quantity.bind(this);
+    this.handleQuantityChange = this.handleQuantityChange.bind(this);
+    this.tradeFee = this.tradeFee.bind(this);
+    this.handleTradeFeeChange = this.handleTradeFeeChange.bind(this);
+    this.tradeDate = this.tradeDate.bind(this);
+    this.handleTradeDateChange = this.handleTradeDateChange.bind(this);
+    this.revertBadFormInput = this.revertBadFormInput.bind(this);
+    this.allFormInputsValid = this.allFormInputsValid.bind(this);
     this.addStock = this.addStock.bind(this);
   }
 
   addStock() {
-    // todo-ww clean this up and add error state
-
-    let transactionType = parseInt(this.state.transactionType, 10);
-    let stock_symbol = this.stockSymbolRef.inputRef.current.value;
-    let cost_per_unit = this.costPerUnitRef.inputRef.current.value;
-    // todo-ww converts cents to integer
-    if (!/\d+(\.\d{1,2})?/.test(cost_per_unit)) {
-      return;
-    } else {
-      let numDecimals = (cost_per_unit.split('.')[1] || []).length;
-      cost_per_unit = Math.round(parseFloat(cost_per_unit, 10) * Math.pow(10, numDecimals));
-    }
-    let quantity = this.quantityRef.inputRef.current.value;
-    if (!/\d+/.test(quantity)) {
-      return;
-    } else {
-      quantity = parseInt(quantity, 10);
-    }
-    let trade_fee = this.tradeFeeRef.inputRef.current.value;
-    // todo-ww converts cents to integer
-    if (!/\d+(\.\d{1,2})?/.test(trade_fee)) {
-      return;
-    } else {
-      let numDecimals = (trade_fee.split('.')[1] || []).length;
-      trade_fee = Math.round(parseFloat(trade_fee, 10) * Math.pow(10, numDecimals));
-    }
-    let trade_date = this.tradeDateRef.inputRef.current.value;
-    // todo-ww proper date validation
-    if (!/\d{4}-\d{2}-\d{2}/.test(trade_date)) {
-      return;
-    }
     this.props.addTransaction({
-      transaction_type: transactionType,
-      stock_symbol: stock_symbol,
-      cost_per_unit: cost_per_unit,
-      quantity: quantity,
-      trade_fee: trade_fee,
-      trade_date: trade_date
+      transaction_type: parseInt(this.transactionType(), 10),
+      stock_symbol: this.stockSymbol(),
+      cost_per_unit: currencyToInteger(this.costPerUnit()),
+      quantity: this.quantity(),
+      trade_fee: currencyToInteger(this.tradeFee()),
+      trade_date: this.tradeDate()
+    });
+    this.setState({
+      addTransactionFields: Object.assign(
+        {}, addTransactionFormInitialState
+      )
     });
     this.close();
   }
@@ -85,28 +101,42 @@ class StockTransactionsContainer extends React.Component {
                open={this.state.open}
                onClose={this.close}>
             <Modal.Header>Add Stock Transaction</Modal.Header>
-            <Modal.Content>
+            <Modal.Content className="add-transaction">
               <Dropdown placeholder="Transaction type"
                         selection
-                        value={this.state.transactionType}
+                        value={this.transactionType()}
                         onChange={this.handleTransactionTypeChange}
                         options={transactionTypes}/>
               <Input placeholder="Stock Symbol"
-                     ref={el => this.stockSymbolRef = el}/>
+                     value={this.stockSymbol()}
+                     onChange={this.handleStockSymbolChange}
+              />
               <Input placeholder="Quantity"
-                     ref={el => this.quantityRef = el}/>
+                     type="number"
+                     value={this.quantity()}
+                     onChange={this.handleQuantityChange}
+              />
               <Input placeholder="Cost per unit"
-                     ref={el => this.costPerUnitRef = el}/>
+                     type="number"
+                     value={this.costPerUnit()}
+                     onChange={this.handleCostPerUnitChange}
+              />
               <Input placeholder="Trade Fee"
-                     ref={el => this.tradeFeeRef = el}/>
+                     type="number"
+                     value={this.tradeFee()}
+                     onChange={this.handleTradeFeeChange}
+              />
               <Input placeholder="Trade Date"
-                     ref={el => this.tradeDateRef = el}/>
+                     value={this.tradeDate()}
+                     onChange={this.handleTradeDateChange}
+              />
             </Modal.Content>
             <Modal.Actions>
               <Button onClick={this.close}>
                 Cancel
               </Button>
               <Button positive
+                      disabled={!this.allFormInputsValid()}
                       onClick={this.addStock}>
                 Create
               </Button>
@@ -115,6 +145,119 @@ class StockTransactionsContainer extends React.Component {
         <Button onClick={this.show}>Add transaction</Button>
       </div>
     );
+  }
+
+  allFormInputsValid() {
+    return this.stockSymbol() !== "" &&
+      this.costPerUnit() !== "" &&
+      this.quantity() !== "" &&
+      this.tradeFee() !== "" &&
+      this.state.addTransactionFields.valid_date;
+  }
+
+  revertBadFormInput() {
+    this.setState({
+      addTransactionFields: Object.assign(
+        {}, this.state.addTransactionFields
+      )
+    });
+  }
+
+  transactionType() {
+    return this.state.addTransactionFields.transactionType;
+  }
+
+  handleTransactionTypeChange(e, { value }) {
+    this.setState({
+      addTransactionFields: Object.assign(
+        {}, this.state.addTransactionFields, {
+          transactionType: value
+        }
+      )
+    });
+  }
+
+  stockSymbol() {
+    return this.state.addTransactionFields.stock_symbol;
+  }
+
+  handleStockSymbolChange(e, { value }) {
+    this.setState({
+      addTransactionFields: Object.assign(
+        {}, this.state.addTransactionFields, {
+          stock_symbol: value
+        }
+      )
+    });
+  }
+
+  costPerUnit() {
+    return this.state.addTransactionFields.cost_per_unit;
+  }
+
+  handleCostPerUnitChange(e, { value }) {
+    if (validCurrency(value)) {
+      this.setState({
+        addTransactionFields: Object.assign(
+          {}, this.state.addTransactionFields, {
+            cost_per_unit: value
+          }
+        )
+      });
+    } else {
+      this.revertBadFormInput();
+    }
+  }
+
+  quantity() {
+    return this.state.addTransactionFields.quantity;
+  }
+
+  handleQuantityChange(e, { value }) {
+    if (validPositiveInteger(value)) {
+      this.setState({
+        addTransactionFields: Object.assign(
+          {}, this.state.addTransactionFields, {
+            quantity: value
+          }
+        )
+      });
+    } else {
+      this.revertBadFormInput();
+    }
+  }
+
+  tradeFee() {
+    return this.state.addTransactionFields.trade_fee;
+  }
+
+  handleTradeFeeChange(e, { value }) {
+    if (validCurrency(value)) {
+      this.setState({
+        addTransactionFields: Object.assign(
+          {}, this.state.addTransactionFields, {
+            trade_fee: value
+          }
+        )
+      });
+    } else {
+      this.revertBadFormInput();
+    }
+  }
+
+  tradeDate() {
+    return this.state.addTransactionFields.trade_date;
+  }
+
+  handleTradeDateChange(e, { value }) {
+    this.setState({
+      addTransactionFields: Object.assign(
+        {}, this.state.addTransactionFields, {
+          trade_date: value,
+          valid_date: validYYYY_MM_DD(value)
+        }
+      )
+    });
   }
 }
 
