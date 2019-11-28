4d9805aeac553d5ffdb755fe1d9c316d786e1e38
Wilson Y. Wong <Wilson.Young.Wong@gmail.com>
2019-11-27 20:46:17 -0800

    Display adjust cost base in table

--- src/components/AdjustCostBaseContainer.js
+++ src/components/AdjustCostBaseContainer.js
@@ -3,15 +3,21 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Table } from 'semantic-ui-react'
+import {
+  getInvestmentAccountAdjustCostBase,
+} from '../actions/InvestmentAccount.js';
 import InvestmentAccountTools from './InvestmentAccountTools';
 
 const mapToStateProps = function(state) {
   return {
+    acb: state.investmentAccountReducer.acb,
+    selectedAccountId: state.investmentAccountReducer.selectedAccountId,
   };
 }
 
 const mapDispatchToProps = function(dispatch) {
   return bindActionCreators({
+    getInvestmentAccountAdjustCostBase: getInvestmentAccountAdjustCostBase,
   }, dispatch);
 }
 
@@ -21,6 +27,11 @@ class AdjustCostBaseContainer extends React.Component {
   }
 
   componentDidUpdate(prevProps) {
+    if (prevProps.selectedAccountId !== this.props.selectedAccountId) {
+      if (this.props.selectedAccountId !== null) {
+        this.props.getInvestmentAccountAdjustCostBase(this.props.selectedAccountId);
+      }
+    }
   }
 
   render() {
@@ -35,6 +46,19 @@ class AdjustCostBaseContainer extends React.Component {
             </Table.Row>
           </Table.Header>
           <Table.Body>
+            {
+              this.props.acb['adjust_cost_base'] ?  Object.keys(
+                this.props.acb['adjust_cost_base']
+              ).map(key => {
+                let stock_acb = this.props.acb['adjust_cost_base'][key];
+                return (
+                  <Table.Row key={key}>
+                    <Table.Cell textAlign="center">{key}</Table.Cell>
+                    <Table.Cell textAlign="center">{stock_acb}</Table.Cell>
+                  </Table.Row>
+                );
+              }) : null
+            }
           </Table.Body>
         </Table>
       </div>
